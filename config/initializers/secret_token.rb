99d6856443cd2bc2e08b9d8955ff15619e1c74c1
ahmed eshra <eng.eshra@gmail.com>
2012-03-30 07:03:16 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoAppEshra::Application.config.secret_token = 'dc79a22da7da93f72886171e588ebcd9112e9c6a7a8f2f2b7f7553772049ac31290f1e098c7fe13ce773bc84f0d5b4e3e16462aa1c8ee536dc2a7438bc945dfc'
